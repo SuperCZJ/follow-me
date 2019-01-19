855f053beb0a2a999abc52c1797cb0f660364408
SuperCZJ <1057236777@qq.com>
2019-01-19 16:13:09 +0800

    es6

--- 移动端/小李子/js/index.js
+++ 移动端/小李子/js/index.js
@@ -1,25 +1,33 @@
-//当前的 划出来
-var  inow = 0;
-//划出去
+// 当前的  划出来
+var iNow = 0;
+// 划出去
 var last = -1;
+
 $(document).swipeUp(function(){
-    last = inow;
-    inow++;
-    if(inow <= $('.page').length - 1){
-        $('.page').eq(last).addClass('moveToTop');
-        $('.page').eq(inow).removeClass('hide').addClass('moveFromBottom');
+    last = iNow;
+    iNow++;
+    if(iNow <= $('.page').length-1){
+        $('.page').eq(last).attr('class','page page-'+ (last +1) +' moveToTop');
+        $('.page').eq(iNow).removeClass('hide').attr('class','page page-'+(iNow+1)+' moveFromBottom');
+
         setTimeout(function(){
-            $('.page').eq(inow).find('img').removeClass('hide');
-        },600)
+            // 等iNow 当前页 600ms划出来后  再让图片出来开始执行动画
+            $('.page').eq(iNow).find('img').removeClass('hide');
+            $('.page').eq(last).find('img').addClass('hide');
+        },600);
     }
-    
-});
+})
 $(document).swipeDown(function(){
-    last = inow;
-    inow--;
-    $('.page').eq(last).attr('class','page page-'+(last+1)+' moveToDown ');
-    $('.page').eq(inow).attr('class','page page-'+(inow+1)+' moveFromTop ')
-    setTimeout(function(){
-        $('.page').eq(inow).find('img').removeClass('hide');
-    },600)
-});
+    if(iNow >=1 ){
+        last = iNow;
+        iNow--;
+        $('.page').eq(last).attr('class','page page-'+ (last+1) +' moveToDown');
+        $('.page').eq(iNow).attr('class','page page-'+ (iNow+1) +' moveFromTop');
+        setTimeout(function(){
+            // 等iNow 当前页 600ms划出来后  再让图片出来开始执行动画
+            $('.page').eq(iNow).find('img').removeClass('hide');
+            $('.page').eq(last).find('img').addClass('hide');
+        },600);
+    }
+})
+
